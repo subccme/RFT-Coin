a55da5e970b1fbf0122f47a779cedd464b26a84b
subccme <117893176+subccme@users.noreply.github.com>
2022-12-21 14:34:09 +0530

    Update Smartcontract.sol

--- Smartcontract.sol
+++ Smartcontract.sol
@@ -25,8 +25,8 @@ contract Store {
         return favoriteNumber;
     }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
+    function addPeople(string memory _name, uint256 _favoriteNumber) public {
+        person.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
